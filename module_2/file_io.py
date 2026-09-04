a89d53463e45f8601a45ddd7252fd7fccdc5127b
julien <julien.stanley13@gmail.com>
2026-09-03 23:20:43 -0600

    file io lab

---
+++ module_2/file_io.py
@@ -0,0 +1,43 @@
+#part1
+
+# f = open('roster.txt', 'w')
+# f.close
+
+
+
+
+with open('roster.txt', 'w') as f:
+    f.write('Alice\n')
+    f.write('Bob\n')
+    f.write('Fred\n')
+
+names = ['Moe\n', 'Larry\n', 'Curly\n']
+with open('roster.txt', 'a') as f:
+    f.writelines(names)
+    
+# print('Done writing')
+
+#reads
+# with open('roster.txt', 'r') as f:
+#     contents = f.readlines()
+# print(contents)
+# print(type(contents))
+#read one line at a time
+
+# with open('roster.txt', 'r') as f:
+#     for line in f:
+#         print(line.strip())
+
+with open('scores.txt', 'r') as f:
+    allScores = f.readlines()
+    intScores = []
+    for s in allScores:
+        intScores.append(int(s.strip()))
+
+total = 0
+for item in intScores:
+    total += item
+    
+print(intScores)
+print(total)
+print(f'the average is {total/len(intScores)}')
